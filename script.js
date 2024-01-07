376c9ae50a07facb443c9de3aacd374bea9993ad
W-Fow <wfowler2020@gmail.com>
2024-01-06 19:30:14 -0500

    Changed from HTML onclick attributes to JavaScript event handlers. Got flag button to work now

--- script.js
+++ script.js
@@ -3,18 +3,38 @@ var boardSize = null;
 var flagMode = false;
 
 function generateBoard(){
-    let numMines = document.getElementById("numMines").value;
-    let size = document.getElementById("boardSize").value;
-    let board_elem = document.getElementById("board");
-    board_elem.style.gridTemplateColumns = `repeat(${size}, 50px)`;
-    board_elem.style.gridTemplateRows = `repeat(${size}, 50px)`;
+    let numMines = document.getElementById("numMines").value; 
+    boardSize = document.getElementById("boardSize").value; 
 
-    mines =create2DArray(size);
-    boardSize = size;
-    createBoard(size);
-    placeMines(size,numMines);
+    let board_elem = document.getElementById("board"); 
+    board_elem.style.gridTemplateColumns = `repeat(${boardSize}, 50px)`;
+    board_elem.style.gridTemplateRows = `repeat(${boardSize}, 50px)`;
+    
+    createBoard(boardSize);
+
+    mines = create2DArray(boardSize);
+    placeMines(boardSize,numMines);
 }
 
+function createBoard(n){
+    const board_elem = document.getElementById("board");
+    board_elem.innerHTML = '';
+    for (let i=0; i<n;i++){
+        for (let j=0;j<n;j++){
+            const button = document.createElement("button");
+            button.setAttribute("class","cell");
+            button.classList.add("hidden");
+            button.setAttribute("id","("+j+","+i+")");
+
+            button.addEventListener('click', handleCellClick);
+            button.addEventListener('contextmenu', handleCellRightClick);
+            
+            //button.setAttribute("onclick","revealElement(this)");
+            //button.setAttribute("oncontextmenu","flag(this);return false;");
+            board_elem.appendChild(button);
+        }
+    }
+}
 function create2DArray (size){
     let array = [];
     for (let i = 0; i < size; i++) {
@@ -26,55 +46,89 @@ function create2DArray (size){
     }
     return array;
 }
-
-function createBoard(n){
-    const board_elem = document.getElementById("board");
-    board_elem.innerHTML = '';
-    for (let i=0; i<n;i++){
-        for (let j=0;j<n;j++){
-            const button = document.createElement("button");
-            button.setAttribute("class","cell");
-            button.setAttribute("id","("+j+","+i+")");
-            button.setAttribute("onclick","revealElement(this)");
-            button.setAttribute("oncontextmenu","flag(this);return false;");
-            board_elem.appendChild(button);
+function placeMines(size,num){
+    for (let i=0;i<num;i++){
+        let x = Math.floor(Math.random()*size);
+        let y = Math.floor(Math.random()*size);
+        if (mines[x][y]==1){
+            i--;
+        }else{
+            mines[x][y]=1;
         }
     }
 }
 
+function handleCellClick(event){
+    const element = event.target;
+    if(element.classList.contains("revealed")){
+        return;
+    }
+    if(flagMode){
+        toggleFlag(element);
+    }else{
+        if(! element.classList.contains("flagged")){
+            revealElement(element);
+        }
+    }
+}
+function handleCellRightClick(event){
+    event.preventDefault();//stop context menu from showing up
+    const element = event.target;
+    if(element.classList.contains("revealed")){
+        return;
+    }
+    if(flagMode){
+        if(! element.classList.contains("flagged")){
+            revealElement(element);
+        }
+    }else{
+        toggleFlag(element);
+    }
+}
+
+function toggleFlag(element){
+    if(element.classList.contains("flagged")){
+        unflag(element);
+    }else{
+        flag(element);
+    }
+}
+
 function flag(el){
-    var element=el;
+    let element=el;
     element.classList.add('flagged');
-    element.setAttribute("oncontextmenu","unflag(this);return false;");
-    element.setAttribute("onclick","");
-    element.disabled = true;
 }
 function unflag(el){
-    var element=el;
+    let element=el;
     element.classList.remove('flagged');
-    element.setAttribute("oncontextmenu","flag(this);return false;");
-    element.setAttribute("onclick","revealElement(this)");
-    element.disabled = false;
 }
+
+function toggleFlagMode(flagButton){
+    flagMode = !flagMode
+    if(flagMode){
+        flagButton.style.backgroundColor= "Crimson";
+    }else{
+        flagButton.style.backgroundColor= "DarkRed";
+    }
+}
+
+
 function revealElement(el){
-    var element=el;
+    let element=el;
     let x = element.id[1];
     let y = element.id[3];
     reveal(element,x,y);
 }
 function revealByCoordinates(x,y){
-    console.log("x:"+x+" y:"+y);
     let element=document.getElementById("("+x+","+y+")");
     reveal(element,x,y);
 }
 function reveal(el,x,y){
     let element=el;
-    element.classList.add('revealed');
-    element.setAttribute("oncontextmenu","");
+    element.classList.replace("hidden","revealed");
     if (mines[x][y]==1){
         element.textContent="boom";
         alert("You lose");
-
     }else{
         nearByBombs=viewNearByCells(isBomb,Number(x),Number(y));
         if (nearByBombs==0){
@@ -90,7 +144,6 @@ function isBomb(x,y){
 }
 
 function viewNearByCells(f,x,y){
-    
     return (safeCheck(f,x-1,y-1)+
             safeCheck(f,x-1,y)+
             safeCheck(f,x-1,y+1)+
@@ -105,7 +158,7 @@ function safeCheck(f,x,y){
     if (x>=boardSize || x<0 || y>=boardSize || y<0){
         return 0;
     }else{
-        element = document.getElementById("("+x+","+y+")");
+        let element = document.getElementById("("+x+","+y+")");
         if(element.classList.contains("revealed")){
             return 0;
         }else{
@@ -114,14 +167,3 @@ function safeCheck(f,x,y){
     }
 }
 
-function placeMines(size,num){
-    for (let i=0;i<num;i++){
-        let x = Math.floor(Math.random()*size);
-        let y = Math.floor(Math.random()*size);
-        if (mines[x][y]==1){
-            i--;
-        }else{
-            mines[x][y]=1;
-        }
-    }
-}
